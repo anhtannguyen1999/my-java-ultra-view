dcdfdb243f4bd212d1cfb4d3bbd47f1801320e4c
Nguyen Anh Tan <anhtannguyen1999@gmail.com>
2019-11-11 18:07:38 +0700

    Fix captureScreen

--- UltraView/src/OS/ScreenCapturer.java
+++ UltraView/src/OS/ScreenCapturer.java
@@ -19,21 +19,36 @@ import javax.swing.ImageIcon;
 
 import DTO.DTO_ArrayLANImageInforObject;
 import DTO.DTO_LANImageInforObject;
+import GUI.RemoteScreenForm;
 
 public class ScreenCapturer {
+	public static void main(String[] args) throws InterruptedException {
+		ScreenCapturer testCaptureScreen=new ScreenCapturer();
+		RemoteScreenForm remoteScreenForm=new RemoteScreenForm("192.169.1.2", "1999", "");
+		while(true) {
+			Thread.sleep(40);
+			DTO_ArrayLANImageInforObject arrayLANImageInforObject=testCaptureScreen.GetLANScreenCaptureImageArray();
+			remoteScreenForm.ShowImageToPanel(arrayLANImageInforObject);
+		}
+		
+	}
+	
+	
 	private BufferedImage screenFullImage;
 	private Robot robot;
 	private Rectangle screenRect;
 	private ImageIcon imgIcon;
 	//private static int count=-1;
-	private byte[] byteImage;
-	private int screenWidth;
-	private int screenHeight;
+	//private byte[] byteImage;
+	//private int screenWidth;
+	//private int screenHeight;
 	private final int numGridMatrixOIP=16;
 	private DTO_LANImageInforObject matrixOIP[][]=new DTO_LANImageInforObject[numGridMatrixOIP][numGridMatrixOIP];
 	private int timeCountMatrixOIP[][]=new int [numGridMatrixOIP][numGridMatrixOIP];
 	private final int maxTimCountMatrixOIP=40;
 	private DTO_ArrayLANImageInforObject arrOIP;
+	private int gridWidth;
+	private int gridHeight;
 	
 	public byte[] CompressImageToByte(BufferedImage image) {
 		// The important part: Create in-memory stream
@@ -71,66 +86,60 @@ public class ScreenCapturer {
 		return null;
 	}
 
+	public ScreenCapturer(){
+		screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        screenFullImage = robot.createScreenCapture(screenRect); //ImageIO.write(screenFullImage, "jpg", new File(fileName));
+		gridWidth=screenFullImage.getWidth()/16;
+   	 	gridHeight=screenFullImage.getHeight()/16;
+   	 	screenFullImage.flush();
+	}
+
 	public DTO_ArrayLANImageInforObject GetLANScreenCaptureImageArray() {
-		robot=null;
-		screenRect=null;
-		//count=0;
-		screenWidth= (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-		screenHeight= (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-		int gridWidth=0;
-		int gridHeight=0;
+        if(screenFullImage!=null) {
+       	 screenFullImage.flush();
+        }
+
+   	 java.lang.Runtime.getRuntime().gc();
+        screenFullImage = robot.createScreenCapture(screenRect); //ImageIO.write(screenFullImage, "jpg", new File(fileName));
         
-        	
-         if(robot==null)
-			try {
-				robot = new Robot();
-			} catch (AWTException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-         //String fileName = "D://FullScreenshot.jpg";
-         if(screenRect==null)
-        	 screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-         if(screenFullImage!=null) {
-        	 java.lang.Runtime.getRuntime().gc();
-        	 screenFullImage.flush();
-         }
-        	
-         screenFullImage = robot.createScreenCapture(screenRect); //ImageIO.write(screenFullImage, "jpg", new File(fileName));
-         if(gridWidth==0||gridHeight==0) {
-        	 gridWidth=screenFullImage.getWidth()/16;
-        	 gridHeight=screenFullImage.getHeight()/16;
-         }
-         ImageIcon imgScrop;
-         arrOIP=new DTO_ArrayLANImageInforObject();
-         for(int i=0;i<16;i++)//row |
-        	 for(int j=0;j<16;j++) { //column ->
-        		 imgScrop = new ImageIcon(screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight));
-        		 //imgScrop=screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight);
-        		 DTO_LANImageInforObject oip=new DTO_LANImageInforObject(imgScrop,j,i,gridWidth,gridHeight);
-        		 if(timeCountMatrixOIP[j][i]==0) {
-        			 matrixOIP[j][i]= oip;
-        			 arrOIP.Add(oip);
-        		 }
-        		 else if(!isEqualToArrElement(oip,j,i)) { //Phai sua lai: neu co thay doi thi cap nhat lai matrix va send di 
-        			 ((DTO_LANImageInforObject)matrixOIP[j][i]).reset();
-        			 matrixOIP[j][i]= oip;
-        			 arrOIP.Add(oip);
-        		 }	
-        		 timeCountMatrixOIP[j][i]++;
-        		 if(timeCountMatrixOIP[j][i]>maxTimCountMatrixOIP)
-        			 timeCountMatrixOIP[j][i]=0;
-        	 }
-         return arrOIP;
-        
-    }
+        ImageIcon imgScrop;
+        arrOIP=new DTO_ArrayLANImageInforObject();
+        for(int i=0;i<numGridMatrixOIP;i++)//row |
+       	 for(int j=0;j<numGridMatrixOIP;j++) { //column ->
+       		 imgScrop = new ImageIcon(screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight));
+       		 //imgScrop=screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight);
+       		 DTO_LANImageInforObject oip=new DTO_LANImageInforObject(imgScrop,j,i,gridWidth,gridHeight);
+       		 if(timeCountMatrixOIP[j][i]==0) {
+       			 matrixOIP[j][i]= oip;
+       			 arrOIP.Add(oip);
+       		 }
+       		 else if(!isEqualToArrElement(oip,j,i)) { //Phai sua lai: neu co thay doi thi cap nhat lai matrix va send di 
+       			 ((DTO_LANImageInforObject)matrixOIP[j][i]).reset();
+       			 matrixOIP[j][i]= oip;
+       			 arrOIP.Add(oip);
+       		 }	
+       		 timeCountMatrixOIP[j][i]++;
+       		 if(timeCountMatrixOIP[j][i]>maxTimCountMatrixOIP)
+       			 timeCountMatrixOIP[j][i]=0;
+       	 }
+        return arrOIP;
+       
+   }
 
 	public boolean isEqualToArrElement(DTO_LANImageInforObject oip, int x,int y) {
+		BufferedImage img1=(BufferedImage)oip.image.getImage();
+		BufferedImage img2=(BufferedImage)matrixOIP[x][y].image.getImage();
 		int w=oip.width, h=oip.height;
-		int wStep=w/16, hStep=h/16;
-		for(int i=0;i<w-1;i+=wStep) {
-			for(int j=0;j<h-1;j+=hStep) {
-				if(((BufferedImage)oip.image.getImage()).getRGB(i,j)!=((BufferedImage)matrixOIP[x][y].image.getImage()).getRGB(i,j)) {
+		int wStep=w/10, hStep=h/10;
+		for(int i=0;i<w-2;i+=wStep) {
+			for(int j=0;j<h-2;j+=hStep) {
+				if(img1.getRGB(i,j)!=img2.getRGB(i,j)) {
 					return false;
 				}
 			}
--- UltraView/src/TestCaptureScreen.java
+++ UltraView/src/TestCaptureScreen.java
@@ -22,35 +22,25 @@ import GUI.RemoteScreenForm;
 public class TestCaptureScreen {
 	public static void main(String[] args) throws InterruptedException {
 		TestCaptureScreen testCaptureScreen=new TestCaptureScreen();
-		RemoteScreenForm remoteScreenForm=new RemoteScreenForm("192.169.1.2", "1999", "");
+		RemoteScreenForm remoteScreenForm=new RemoteScreenForm("192.169.1.2", "1999", "ahihi");
 		while(true) {
 			Thread.sleep(40);
-			DTO_ArrayLANImageInforObject arrayLANImageInforObject=testCaptureScreen.GetLANScreenCaptureImageArray1();
+			DTO_ArrayLANImageInforObject arrayLANImageInforObject=testCaptureScreen.GetLANScreenCaptureImageArray();
 			remoteScreenForm.ShowImageToPanel(arrayLANImageInforObject);
 		}
-		
 	}
 	private BufferedImage screenFullImage;
 	private Robot robot;
-	private Rectangle screenRect=null;
-	//private ImageIcon imgIcon;
-	//private static int count=-1;
-	//private byte[] byteImage;
-	//private int screenWidth;
-	//private int screenHeight;
-	
-	private final int colRowNum=16;
-	
-	private DTO_LANImageInforObject matrixlanImgInfoObj[][]=new DTO_LANImageInforObject[colRowNum][colRowNum];
-	private int timeCountMatrixlanImgInfoObj[][]=new int [colRowNum][colRowNum];
-	private final int maxTimCountMatrixlanImgInfoObj=40;
-	private DTO_ArrayLANImageInforObject arrlanImgInfoObj;
+	private Rectangle screenRect;
+	private final int numGridMatrixOIP=16;
+	private DTO_LANImageInforObject matrixOIP[][]=new DTO_LANImageInforObject[numGridMatrixOIP][numGridMatrixOIP];
+	private int timeCountMatrixOIP[][]=new int [numGridMatrixOIP][numGridMatrixOIP];
+	private final int maxTimCountMatrixOIP=40;
+	private DTO_ArrayLANImageInforObject arrOIP;
 	private int gridWidth;
 	private int gridHeight;
 	
 	TestCaptureScreen(){
-		//screenWidth= (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-		//screenHeight= (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 		try {
 			robot = new Robot();
@@ -58,67 +48,50 @@ public class TestCaptureScreen {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		screenFullImage = robot.createScreenCapture(screenRect);
-		if(gridWidth==0||gridHeight==0) {
-	       	 gridWidth=screenFullImage.getWidth()/colRowNum;
-	       	 gridHeight=screenFullImage.getHeight()/colRowNum;
-        }
-		wStep=gridWidth/colRowNum; hStep=gridHeight/colRowNum;
+        screenFullImage = robot.createScreenCapture(screenRect); //ImageIO.write(screenFullImage, "jpg", new File(fileName));
+		gridWidth=screenFullImage.getWidth()/16;
+   	 	gridHeight=screenFullImage.getHeight()/16;
 	}
-	//10%-25%CPU, TB 15%
-	public DTO_ArrayLANImageInforObject GetLANScreenCaptureImageArray1() {
-		
- 
-         //String fileName = "D://FullScreenshot.jpg";
-		if(arrlanImgInfoObj!=null)
-			arrlanImgInfoObj.Clear();
-		else {
-			arrlanImgInfoObj=new DTO_ArrayLANImageInforObject();
-		}
-	     if(screenFullImage!=null) {
-	    	 screenFullImage.flush();
-	    	 
-	     }
-	     java.lang.Runtime.getRuntime().gc();	
+	
+	public DTO_ArrayLANImageInforObject GetLANScreenCaptureImageArray() {
+         if(screenFullImage!=null) {
+        	 screenFullImage.flush();
+         }
+
+    	 java.lang.Runtime.getRuntime().gc();
          screenFullImage = robot.createScreenCapture(screenRect); //ImageIO.write(screenFullImage, "jpg", new File(fileName));
          
          ImageIcon imgScrop;
-         //arrlanImgInfoObj=new DTO_ArrayLANImageInforObject();
-         for(int i=0;i<colRowNum;i++)//row |
-        	 for(int j=0;j<colRowNum;j++) { //column ->
+         arrOIP=new DTO_ArrayLANImageInforObject();
+         for(int i=0;i<16;i++)//row |
+        	 for(int j=0;j<16;j++) { //column ->
         		 imgScrop = new ImageIcon(screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight));
         		 //imgScrop=screenFullImage.getSubimage(j*gridWidth, i*gridHeight,gridWidth , gridHeight);
-        		 DTO_LANImageInforObject lanImgInfoObj=new DTO_LANImageInforObject(imgScrop,j,i,gridWidth,gridHeight);
-        		 
-        		 //Vua moi reset count
-        		 if(timeCountMatrixlanImgInfoObj[j][i]==0) {
-        			 matrixlanImgInfoObj[j][i]= lanImgInfoObj; //Gan image obj
-        			 arrlanImgInfoObj.Add(lanImgInfoObj);	//Add vo array list
+        		 DTO_LANImageInforObject oip=new DTO_LANImageInforObject(imgScrop,j,i,gridWidth,gridHeight);
+        		 if(timeCountMatrixOIP[j][i]==0) {
+        			 matrixOIP[j][i]= oip;
+        			 arrOIP.Add(oip);
         		 }
-        		 //Neu khong bi trung
-        		 else if(!isEqualToArrElement1(lanImgInfoObj,j,i)) { 
-        			 matrixlanImgInfoObj[j][i].reset();
-        			 matrixlanImgInfoObj[j][i]= lanImgInfoObj;
-        			 arrlanImgInfoObj.Add(lanImgInfoObj);
-        		 }
-        		 else {
-        			 timeCountMatrixlanImgInfoObj[j][i]++;
-            		 if(timeCountMatrixlanImgInfoObj[j][i]>maxTimCountMatrixlanImgInfoObj)
-            			 timeCountMatrixlanImgInfoObj[j][i]=0;
-        		 }
-        			 
+        		 else if(!isEqualToArrElement(oip,j,i)) { //Phai sua lai: neu co thay doi thi cap nhat lai matrix va send di 
+        			 ((DTO_LANImageInforObject)matrixOIP[j][i]).reset();
+        			 matrixOIP[j][i]= oip;
+        			 arrOIP.Add(oip);
+        		 }	
+        		 timeCountMatrixOIP[j][i]++;
+        		 if(timeCountMatrixOIP[j][i]>maxTimCountMatrixOIP)
+        			 timeCountMatrixOIP[j][i]=0;
         	 }
-         return arrlanImgInfoObj;
+         return arrOIP;
         
     }
 
-	private int wStep;
-	private int hStep;
-	public boolean isEqualToArrElement1(DTO_LANImageInforObject lanImgInfoObj, int x,int y) {
-		BufferedImage img1=(BufferedImage)lanImgInfoObj.image.getImage();
-		BufferedImage img2=(BufferedImage)matrixlanImgInfoObj[x][y].image.getImage();
-		for(int i=0;i<gridWidth-2;i+=wStep) {
-			for(int j=0;j<gridHeight-2;j+=hStep) {
+	public boolean isEqualToArrElement(DTO_LANImageInforObject oip, int x,int y) {
+		BufferedImage img1=(BufferedImage)oip.image.getImage();
+		BufferedImage img2=(BufferedImage)matrixOIP[x][y].image.getImage();
+		int w=oip.width, h=oip.height;
+		int wStep=w/8, hStep=h/8;
+		for(int i=0;i<w-2;i+=wStep) {
+			for(int j=0;j<h-2;j+=hStep) {
 				if(img1.getRGB(i,j)!=img2.getRGB(i,j)) {
 					return false;
 				}
