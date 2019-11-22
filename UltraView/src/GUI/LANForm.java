3df61764120f1d582cd78ed400603496fbad5ebf
Nguyen Anh Tan <anhtannguyen1999@gmail.com>
2019-11-23 00:06:06 +0700

    Design GUI

--- UltraView/src/GUI/LANForm.java
+++ UltraView/src/GUI/LANForm.java
@@ -16,6 +16,7 @@ import javax.swing.JMenu;
 import javax.swing.JTabbedPane;
 import javax.swing.JSplitPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
@@ -32,6 +33,17 @@ import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.SystemColor;
+import java.awt.Window.Type;
 
 public class LANForm extends JFrame {
 
@@ -86,6 +98,10 @@ public class LANForm extends JFrame {
 	 * Create the frame.
 	 */
 	public LANForm() {
+		setResizable(false);
+		setTitle("Ultra View");
+		setBackground(Color.LIGHT_GRAY);
+		setForeground(new Color(191, 205, 219));
 		bll_LANForm=new BLL_LANForm();
 		//Event
 		addWindowListener(new WindowAdapter() {
@@ -110,137 +126,62 @@ public class LANForm extends JFrame {
 		});
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		setBounds(100, 100, 650, 375);
+		setBounds(100, 100, 806, 414);
 		
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		setJMenuBar(menuBar);
 		
 		JMenu mnFile = new JMenu("File");
+		mnFile.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		menuBar.add(mnFile);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.inactiveCaption);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
+		JPanel statusPanel = new JPanel();
+		statusPanel.setBackground(SystemColor.inactiveCaption);
+		statusPanel.setBounds(5, 335, 813, 20);
+		statusPanel.setFocusable(false);
+		statusPanel.setAlignmentY(0.0f);
+		statusPanel.setAlignmentX(0.0f);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(248, 248, 255));
+		panel.setBounds(5, 5, 789, 329);
+		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		
 		JPanel pnlOpenCnn = new JPanel();
-		pnlOpenCnn.setBorder(new MatteBorder(0, 0, 0, 1, (Color) new Color(0, 0, 0)));
-		
-		JPanel pnlRemoteForm = new JPanel();
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(pnlOpenCnn, GroupLayout.PREFERRED_SIZE, 318, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(pnlRemoteForm, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(pnlRemoteForm, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
-						.addComponent(pnlOpenCnn, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 248, Short.MAX_VALUE))
-					.addGap(34))
-		);
-		
-		JLabel lblNewLabel_5 = new JLabel("Remote PC");
-		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		
-		JLabel lblNewLabel_7 = new JLabel("Partner IP");
-		
-		JLabel lblNewLabel_8 = new JLabel("Partner Port");
-		
-		JLabel lblNewLabel_9 = new JLabel("Password");
-		
-		txtPartnerIP = new JTextField();
-		txtPartnerIP.setColumns(10);
-		
-		txtPartnerPort = new JTextField();
-		txtPartnerPort.setColumns(10);
-		
-		txtPartnerPassword = new JTextField();
-		txtPartnerPassword.setColumns(10);
-		
-		JButton btnConnect = new JButton("Connect");
-		btnConnect.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				bll_LANForm.ConnectAndShowRemoteForm(txtPartnerIP.getText(),txtPartnerPort.getText(),txtPartnerPassword.getText());
-			}
-		});
-		GroupLayout gl_pnlRemoteForm = new GroupLayout(pnlRemoteForm);
-		gl_pnlRemoteForm.setHorizontalGroup(
-			gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-					.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-							.addGap(117)
-							.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_6)
-								.addComponent(lblNewLabel_5)))
-						.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-							.addContainerGap()
-							.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_7)
-								.addComponent(lblNewLabel_8)
-								.addComponent(lblNewLabel_9))
-							.addGap(37)
-							.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(txtPartnerPassword)
-								.addComponent(btnConnect, Alignment.TRAILING)
-								.addComponent(txtPartnerIP, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
-								.addComponent(txtPartnerPort))))
-					.addContainerGap(34, Short.MAX_VALUE))
-		);
-		gl_pnlRemoteForm.setVerticalGroup(
-			gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-					.addComponent(lblNewLabel_5)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblNewLabel_6)
-					.addGap(40)
-					.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_7)
-						.addComponent(txtPartnerIP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_8)
-						.addComponent(txtPartnerPort, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-							.addGap(61)
-							.addComponent(btnConnect))
-						.addGroup(gl_pnlRemoteForm.createSequentialGroup()
-							.addGap(18)
-							.addGroup(gl_pnlRemoteForm.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel_9)
-								.addComponent(txtPartnerPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-					.addContainerGap(59, Short.MAX_VALUE))
-		);
-		pnlRemoteForm.setLayout(gl_pnlRemoteForm);
-		
-		JLabel lblNewLabel = new JLabel("OpenConnect");
-		
-		JLabel lblNewLabel_1 = new JLabel("M\u00F4 t\u1EA3");
+		pnlOpenCnn.setBackground(new Color(248, 248, 255));
+		pnlOpenCnn.setBorder(new MatteBorder(0, 0, 0, 0, (Color) new Color(0, 0, 0)));
 		
 		JLabel lblNewLabel_2 = new JLabel("Your IP");
+		lblNewLabel_2.setAlignmentY(Component.TOP_ALIGNMENT);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		JLabel lblNewLabel_3 = new JLabel("Your Port");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		txtYourIP = new JTextField();
+		txtYourIP.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtYourIP.setEditable(false);
 		txtYourIP.setColumns(10);
 		
 		txtYourPort = new JTextField();
+		txtYourPort.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtYourPort.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Password");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		txtYourPassword = new JTextField();
+		txtYourPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtYourPassword.setColumns(10);
 		
-		JButton btnOpenConnect = new JButton("Open Connection");
+		JButton btnOpenConnect = new JButton("Allow Connection");
+		btnOpenConnect.setFocusable(false);
+		btnOpenConnect.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnOpenConnect.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -267,57 +208,61 @@ public class LANForm extends JFrame {
 		});
 		
 		JButton btnOpenchat = new JButton("OpenChat");
+		btnOpenchat.setFocusable(false);
+		btnOpenchat.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnOpenchat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				bll_LANForm.ReOpenChat();
 			}
 		});
+		
+		JLabel lblNewLabel_1 = new JLabel("<html>Please click \"Allow Connection\" and send your IP, port, password to your partner if you want they to remote your computer.</html>");
+		lblNewLabel_1.setBackground(new Color(248, 248, 255));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		lblNewLabel_1.setOpaque(true);
 		GroupLayout gl_pnlOpenCnn = new GroupLayout(pnlOpenCnn);
 		gl_pnlOpenCnn.setHorizontalGroup(
 			gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_pnlOpenCnn.createSequentialGroup()
+					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-							.addGap(116)
-							.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_1)
-								.addComponent(lblNewLabel)))
+							.addContainerGap()
+							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 337, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-							.addGap(21)
+							.addGap(31)
 							.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.TRAILING)
 								.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-									.addComponent(btnOpenchat)
+									.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel_4)
+										.addComponent(lblNewLabel_2))
+									.addGap(19))
+								.addGroup(gl_pnlOpenCnn.createSequentialGroup()
+									.addComponent(lblNewLabel_3)
+									.addGap(18)))
+							.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.TRAILING)
+								.addComponent(txtYourIP, GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+								.addGroup(gl_pnlOpenCnn.createSequentialGroup()
+									.addComponent(btnOpenchat, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
 									.addPreferredGap(ComponentPlacement.RELATED)
 									.addComponent(btnOpenConnect))
-								.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-									.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblNewLabel_3)
-										.addComponent(lblNewLabel_2)
-										.addComponent(lblNewLabel_4))
-									.addGap(18)
-									.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.TRAILING)
-										.addComponent(txtYourPassword, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
-										.addComponent(txtYourIP, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
-										.addComponent(txtYourPort, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE))
-									.addPreferredGap(ComponentPlacement.RELATED)))))
-					.addContainerGap(60, GroupLayout.PREFERRED_SIZE))
+								.addComponent(txtYourPassword, 238, 238, 238)
+								.addComponent(txtYourPort, 238, 238, 238))))
+					.addGap(28))
 		);
 		gl_pnlOpenCnn.setVerticalGroup(
-			gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
+			gl_pnlOpenCnn.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_pnlOpenCnn.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGap(5)
 					.addComponent(lblNewLabel_1)
-					.addGap(18)
-					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_2)
-						.addComponent(txtYourIP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_3)
-						.addComponent(txtYourPort, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtYourIP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2))
 					.addGap(18)
+					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtYourPort, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_3))
+					.addGap(13)
 					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNewLabel_4)
 						.addComponent(txtYourPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
@@ -325,9 +270,141 @@ public class LANForm extends JFrame {
 					.addGroup(gl_pnlOpenCnn.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnOpenConnect)
 						.addComponent(btnOpenchat))
-					.addContainerGap(32, Short.MAX_VALUE))
+					.addContainerGap())
 		);
 		pnlOpenCnn.setLayout(gl_pnlOpenCnn);
-		contentPane.setLayout(gl_contentPane);
+		
+		JPanel pnlRemoteForm = new JPanel();
+		pnlRemoteForm.setBackground(new Color(248, 248, 255));
+		pnlRemoteForm.setBorder(new MatteBorder(0, 1, 0, 0, (Color) new Color(0, 0, 0)));
+		
+		JLabel lblNewLabel_7 = new JLabel("Partner IP");
+		lblNewLabel_7.setBounds(13, 84, 71, 20);
+		lblNewLabel_7.setAlignmentY(Component.TOP_ALIGNMENT);
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel lblNewLabel_8 = new JLabel("Partner Port");
+		lblNewLabel_8.setBounds(13, 124, 85, 20);
+		lblNewLabel_8.setAlignmentY(Component.TOP_ALIGNMENT);
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel lblNewLabel_9 = new JLabel("Password");
+		lblNewLabel_9.setBounds(13, 164, 67, 20);
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		txtPartnerIP = new JTextField();
+		txtPartnerIP.setBounds(118, 81, 216, 26);
+		txtPartnerIP.setAlignmentX(Component.LEFT_ALIGNMENT);
+		txtPartnerIP.setAlignmentY(Component.TOP_ALIGNMENT);
+		txtPartnerIP.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtPartnerIP.setColumns(10);
+		
+		txtPartnerPort = new JTextField();
+		txtPartnerPort.setBounds(118, 121, 216, 26);
+		txtPartnerPort.setAlignmentX(Component.LEFT_ALIGNMENT);
+		txtPartnerPort.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		txtPartnerPort.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtPartnerPort.setColumns(10);
+		
+		txtPartnerPassword = new JTextField();
+		txtPartnerPassword.setBounds(119, 161, 216, 26);
+		txtPartnerPassword.setAlignmentY(Component.TOP_ALIGNMENT);
+		txtPartnerPassword.setAlignmentX(Component.LEFT_ALIGNMENT);
+		txtPartnerPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtPartnerPassword.setColumns(10);
+		
+		JButton btnConnect = new JButton("Start Remote");
+		btnConnect.setBounds(192, 200, 142, 29);
+		btnConnect.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		btnConnect.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		btnConnect.setFocusable(false);
+		btnConnect.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnConnect.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				bll_LANForm.ConnectAndShowRemoteForm(txtPartnerIP.getText(),txtPartnerPort.getText(),txtPartnerPassword.getText());
+			}
+		});
+		
+		JLabel lblNewLabel_6 = new JLabel("<html>Please enter IP, port, password of the computer that you want to remote.</html>");
+		lblNewLabel_6.setBackground(new Color(248, 248, 255));
+		lblNewLabel_6.setBounds(43, 13, 284, 40);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		lblNewLabel_6.setVerticalTextPosition(SwingConstants.TOP);
+		lblNewLabel_6.setOpaque(true);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.inactiveCaption);
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		
+		JLabel lblNewLabel = new JLabel("Allow Connection");
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(SystemColor.inactiveCaption);
+		panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		
+		JLabel lblNewLabel_5 = new JLabel("Remote Another");
+		panel_2.add(lblNewLabel_5);
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 20));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(pnlOpenCnn, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 383, Short.MAX_VALUE))
+					.addGap(18)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(pnlRemoteForm, GroupLayout.PREFERRED_SIZE, 352, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(pnlOpenCnn, GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+						.addComponent(pnlRemoteForm, GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		pnlRemoteForm.setLayout(null);
+		pnlRemoteForm.add(lblNewLabel_6);
+		pnlRemoteForm.add(lblNewLabel_9);
+		pnlRemoteForm.add(lblNewLabel_7);
+		pnlRemoteForm.add(lblNewLabel_8);
+		pnlRemoteForm.add(txtPartnerIP);
+		pnlRemoteForm.add(txtPartnerPort);
+		pnlRemoteForm.add(txtPartnerPassword);
+		pnlRemoteForm.add(btnConnect);
+		panel.setLayout(gl_panel);
+		
+		JLabel lblStatus = new JLabel("Status: ...");
+		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GroupLayout gl_statusPanel = new GroupLayout(statusPanel);
+		gl_statusPanel.setHorizontalGroup(
+			gl_statusPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_statusPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblStatus)
+					.addContainerGap(748, Short.MAX_VALUE))
+		);
+		gl_statusPanel.setVerticalGroup(
+			gl_statusPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_statusPanel.createSequentialGroup()
+					.addComponent(lblStatus)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		statusPanel.setLayout(gl_statusPanel);
+		contentPane.setLayout(null);
+		contentPane.add(panel);
+		contentPane.add(statusPanel);
 	}
 }
